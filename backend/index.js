86c4870601c4ae6d6b31ea40179537fbc15fa0ac
Bhavikagire <bhavikagire3035@gmail.com>
2023-10-18 17:08:27 +0530

    server is running

--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,47 @@
-const express = require("express")
-const app = express()
-require('dotenv').config()
-const Port = process.env.Port
+// const express = require("express")
+// const app = express()
+// require('dotenv').config()
+// const Port = process.env.Port
 
-app.get("/",(req, res)=>{
-    res.send("Home page")
-})
+// app.get("/",(req, res)=>{
+//     res.send("Home page")
+// })
 
-app.listen(Port,()=>{
-   console.log(`server is running at ${Port}`)
-})
+// app.listen(Port,()=>{
+//    console.log(`server is running at ${Port}`)
+// })
+
+const express = require('express');
+const mongoose = require('mongoose');
+const config = require('./config/config');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Middleware
+app.use(express.json());
+
+// Connect to MongoDB
+mongoose.connect(config.mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log('MongoDB connected'))
+  .catch(err => console.error(err));
+
+// Load routes
+const authenticationRoutes = require('./routes/authenticationRoutes');
+const restaurantRoutes = require('./routes/restaurantRoutes');
+const orderRoutes = require('./routes/orderRoutes');
+const userRoutes = require('./routes/userRoutes');
+const reviewRoutes = require('./routes/reviewRoutes');
+const passwordResetRoutes = require('./routes/passwordResetRoutes');
+
+// Use routes
+app.use('/api/auth', authenticationRoutes);
+app.use('/api/restaurants', restaurantRoutes);
+app.use('/api/orders', orderRoutes);
+app.use('/api/users', userRoutes);
+app.use('/api/reviews', reviewRoutes);
+app.use('/api/auth', passwordResetRoutes);
+
+app.listen(PORT, () => {
+  console.log(`Server started on port ${PORT}`);
+});
