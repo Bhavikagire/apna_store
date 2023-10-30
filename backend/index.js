163dadb0aca4421b672b4af8509f7d4c45a3c0e3
bhavikagire <bhavikagire3035@gmail.com>
2023-10-30 19:18:52 +0530

    route

--- backend/index.js
+++ backend/index.js
@@ -31,37 +31,37 @@ app.listen(Port,()=>{
 
 
 
-// const express = require('express');
-// const mongoose = require('mongoose');
-// // const config = require('./config/config');
+const express = require('express');
+const mongoose = require('mongoose');
+// const config = require('./config/config');
 
 // const app = express();
-// const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3000;
 
-// // Middleware
-// app.use(express.json());
+// Middleware
+app.use(express.json());
 
-// // Connect to MongoDB
-// // mongoose.connect(config.mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
-// //   .then(() => console.log('MongoDB connected'))
-// //   .catch(err => console.error(err));
+// Connect to MongoDB
+// mongoose.connect(config.mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
+//   .then(() => console.log('MongoDB connected'))
+//   .catch(err => console.error(err));
 
-// // Load routes
-// const authenticationRoutes = require('./routes/authenticationRoutes');
-// const restaurantRoutes = require('./routes/restaurantRoutes');
-// const orderRoutes = require('./routes/orderRoutes');
-// const userRoutes = require('./routes/userRoutes');
-// const reviewRoutes = require('./routes/reviewRoutes');
-// const passwordResetRoutes = require('./routes/passwordResetRoutes');
+// Load routes
+const authenticationRoutes = require('./routes/authenticationRoutes');
+const restaurantRoutes = require('./routes/restaurantRoutes');
+const orderRoutes = require('./routes/orderRoutes');
+const userRoutes = require('./routes/userRoutes');
+const reviewRoutes = require('./routes/reviewRoutes');
+const passwordResetRoutes = require('./routes/passwordResetRoutes');
 
-// // Use routes
-// app.use('/api/auth', authenticationRoutes);
-// app.use('/api/restaurants', restaurantRoutes);
-// app.use('/api/orders', orderRoutes);
-// app.use('/api/users', userRoutes);
-// app.use('/api/reviews', reviewRoutes);
-// app.use('/api/auth', passwordResetRoutes);
+// Use routes
+app.use('/api/auth', authenticationRoutes);
+app.use('/api/restaurants', restaurantRoutes);
+app.use('/api/orders', orderRoutes);
+app.use('/api/users', userRoutes);
+app.use('/api/reviews', reviewRoutes);
+app.use('/api/auth', passwordResetRoutes);
 
-// app.listen(PORT, () => {
-//   console.log(`Server started on port ${PORT}`);
-// });
+app.listen(PORT, () => {
+  console.log(`Server started on port ${PORT}`);
+});
